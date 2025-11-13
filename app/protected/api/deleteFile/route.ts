b466c5477b446578c0fae34fb18913fee4338d06
Finn Healy <healyfinn978@gmail.com>
2025-11-13 04:29:04 +0000

    delete file functionality updated to autheticate user

--- app/protected/api/deleteFile/route.ts
+++ app/protected/api/deleteFile/route.ts
@@ -1,16 +1,21 @@
 // app/api/list-files/route.ts
 import { NextRequest } from "next/server";
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@/lib/supabase/server";
 
-const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!  // use service role key here
-);
+
+
 export async function GET(req: NextRequest) {
+    const supabase = await createClient();
+    const { data: d, error: e } = await supabase.auth.getUser();
+
+    if (e || !d?.user) {
+        return new Response("Unauthorized", { status: 401 });
+    }
     const filename = req.nextUrl.searchParams.get("name"); // get the query param
+    console.log([`uploads/${d.user.email}/${filename}`])
     const { data, error } = await supabase.storage
         .from("user-files")
-        .remove([`uploads/finnhealy113@gmail.com/${filename}`]);
+        .remove([`uploads/${d.user.email}/${filename}`]);
     console.log(data)
     console.log(error)
     return new Response(JSON.stringify({ data, error }));
